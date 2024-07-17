4aed332f59792309b0e834f40b4029619942b12e
Vlad <160039489+55kt@users.noreply.github.com>
2024-07-17 20:21:07 +0300

    first commit

---
+++ WhatsAppClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhatsAppClone
+//
+//  Created by Vlad on 17/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhatsAppClone/WhatsAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppCloneApp.swift
+//  WhatsAppClone
+//
+//  Created by Vlad on 17/7/24.
+//
+
+import SwiftUI
+
+@main
+struct WhatsAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
