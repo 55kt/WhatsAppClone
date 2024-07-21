5ebc09002f8eade248868b96544fd15660b3be94
Vlad <160039489+55kt@users.noreply.github.com>
2024-07-21 18:49:59 +0300

    connection firebase

--- WhatsAppClone/WhatsAppCloneApp.swift
+++ WhatsAppClone/WhatsAppCloneApp.swift
@@ -6,12 +6,26 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct WhatsAppCloneApp: App {
-    var body: some Scene {
-        WindowGroup {
-            MainTabView()
-        }
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
+  var body: some Scene {
+    WindowGroup {
+        MainTabView()
     }
+  }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
 }
